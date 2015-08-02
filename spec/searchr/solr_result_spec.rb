b53393c9f659140ed0688200c330a56d42e8a253
bmd <brian@murphydye.com>
2015-08-02 09:00:48 -0600

    Get solr_result tests passing again.

--- spec/searchr/solr_result_spec.rb
+++ spec/searchr/solr_result_spec.rb
@@ -15,7 +15,7 @@ describe Searchr::SolrResult do
   end
 
   it 'should know number of rows' do
-    expect(@result.num_rows).to eq(10)
+    expect(@result.num_documents_requested).to eq(10)
   end
 
   it 'should have a lot of search results' do
@@ -24,6 +24,7 @@ describe Searchr::SolrResult do
 
   it 'should have 10 documents' do
     expect(@result.documents.size).to eq(10)
+    expect(@result.num_documents_received).to eq(10)
   end
 
   it 'should have explanations' do
@@ -33,16 +34,16 @@ describe Searchr::SolrResult do
   context 'first explanation' do
     let(:explanation) { @result.explanations.first[1] }
 
-    it 'should have two elements' do
-      expect(explanation.size).to eq(2)
+    it 'should have many elements' do
+      expect(explanation.size).to be >5
     end
 
-    it 'first element should be a string' do
-      expect(explanation[0].class).to eq(String)
+    it 'first element should be an explanation' do
+      expect(explanation[0].class).to eq(Searchr::Explanation)
     end
 
-    it 'second element should be an array' do
-      expect(explanation[1].class).to eq(Array)
+    it 'second element should be a string' do
+      expect(explanation[1].class).to eq(String)
       expect(explanation[1].size).to be >3
     end
   end
